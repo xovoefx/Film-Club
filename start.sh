3d8ee85f5c3f0f6b2abe56aeafe0c01fc769aa6d
xovoefx <98139422+xovoefx@users.noreply.github.com>
2022-11-30 19:02:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Samantha-a/Film-Club /Film-Club
+  git clone https://github.com/xovoefx/Film-Club /Film-Club
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Film-Club
